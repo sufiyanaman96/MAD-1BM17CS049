c65b166c2ce61a643f7971d357aebb3c1fd5fe5a
sufiyanaman96 <mohammedsufiyan.cs17@bmsce.ac.in>
2020-03-23 11:55:27 +0530

    updated

---
+++ program7/app/src/main/java/com/example/program7/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.program7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.os.Bundle;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import com.example.program7.GPSTracer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button button;
+        final TextView textView;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GPSTracer gps = new GPSTracer(MainActivity.this);
+                if (gps.getLocation() != null) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    Toast.makeText(getApplicationContext(), "Your Location is \nLat:" + latitude + "\nLong:" + longitude, Toast.LENGTH_LONG).show();
+                    textView.setText("Your Location is ");
+                }
+            }
+        });
+    }
+}
